11d17434e6c85651715b0d75f704322a9a55ad78
CABottega <adamczykchris@gmail.com>
2020-02-03 12:20:44 -0500

    ecommerce application

---
+++ src/components/auth/signup.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+
+import SignUpForm from './signupForm';
+import PageTitle from '../pageTitle';
+
+class Signup extends Component {
+    
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
+    render() {
+        return (
+            <div className='sign-up'>
+                <PageTitle className='sign-up__page-title' title='Register' />
+                <SignUpForm 
+                    className='sign-up__form'
+                    onSubmit={this.onSubmit} 
+                />
+            </div>
+        )
+    }
+}
+
+export default Signup;
---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form';
+
+import HeaderNavbar from './headernavbarReducer';
+
+
+const rootReducer = combineReducers({
+  form,
+  HeaderNavbar
+});
+
+export default rootReducer;
