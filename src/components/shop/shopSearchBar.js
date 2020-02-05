32b52d3d29c61eabf8cc3c5a55b936924d118753
CABottega <adamczykchris@gmail.com>
2020-02-05 17:54:16 -0500

    removed duplicative reference to icon in code on shopSearchBar js file

--- src/components/shop/shopSearchBar.js
+++ src/components/shop/shopSearchBar.js
@@ -6,13 +6,10 @@ function FormSearchBar(props) {
     return (
         <div className='search-bar-grid'>
             <i className="fas fa-search search-bar-grid__icon"></i>
-            {/* <i className="fas fa-search search-bar-grid__icon"></i> */}
             <input className={`${props.className} form-search-bar search-bar-grid__input`} {...props.input} type='text' placeholder={`${props.placeholder}`} />
         </div>    
     )
 }
-
-{/* <i class="fas fa-search"></i> */}
 
 class ShopSearchBar extends Component {
     render() {
