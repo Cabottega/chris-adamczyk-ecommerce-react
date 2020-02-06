edd4128c6b1c357f895970ff9aeecc06b6b11ed9
CABottega <adamczykchris@gmail.com>
2020-02-06 16:27:43 -0500

    created shop cart grid

--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -50,7 +50,7 @@ class ShopCart extends Component {
         return (
             <div className={`${className} shop-cart`}>
                 <CartButton className='shop-cart__toggle' icon='fas fa-times'/>
-                <CartContent className='shop-cart__content' products={[243, 3434, 4554]}/>
+                <CartContent className='shop-cart__content' products={[243, 3434, 4554, 343, 4434, 5554, 4435, 5555, 22243, 33434, 44554]}/>
             </div>
         )
     }
