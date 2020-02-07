5e21d12b99ba6b9407986b5e590568e3adc44786
CABottega <adamczykchris@gmail.com>
2020-02-06 19:05:43 -0500

    cart animations and cart button in shop component

--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -2,16 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import CartProduct from './cartProduct';
+import CartButton from './cartButton';
 import * as actions from '../../actions';
 
-
-function CartButton({className, icon}) {
-    return (
-        <div className={`${className} cart-button`}>
-            <i className={icon}/>        
-        </div>
-    )
-}
 
 function CartContent({className, products}) {
     let count = products.length;
@@ -54,11 +47,19 @@ class ShopCart extends Component {
         this.props.fetchCartProducts();
     }
 
+    handleAddToCart = () => {
+        if(document.getElementById('shop-cart').classList.contains('cart-hidden')) {
+            document.getElementById('shop-cart').classList.remove('cart-hidden');
+        } else {
+            document.getElementById('shop-cart').classList.add('cart-hidden');
+        }
+    }
+
     render() {
         const { className } = this.props;
         return (
-            <div className={`${className} shop-cart`}>
-                <CartButton className='shop-cart__toggle' icon='fas fa-times'/>
+            <div id='shop-cart' className={`${className} shop-cart cart-hidden`}>
+            <CartButton className='shop-cart__toggle' icon='fas fa-times' onClick={this.handleAddToCart}/>
                 <CartContent className='shop-cart__content' products={this.props.cartProducts}/>
             </div>
         )
